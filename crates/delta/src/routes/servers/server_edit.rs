f5cd8402304c537c74ccc9850a57f6282a0be4f3
Zomatree <me@zomatree.live>
2026-04-14 01:59:48 +0100

    fix: remove reference to authifier
    Signed-off-by: Zomatree <me@zomatree.live>

--- crates/delta/src/routes/servers/server_edit.rs
+++ crates/delta/src/routes/servers/server_edit.rs
@@ -1,12 +1,13 @@
 use std::collections::HashSet;
 
 use revolt_database::{
-    Account, Database, File, PartialServer, User, ValidatedTicket, util::{permissions::DatabasePermissionQuery, reference::Reference}
+    util::{permissions::DatabasePermissionQuery, reference::Reference},
+    Database, File, PartialServer, User, ValidatedTicket,
 };
 use revolt_models::v0;
 use revolt_permissions::{calculate_server_permissions, ChannelPermission};
 use revolt_result::{create_error, Result};
-use rocket::{serde::json::Json, Request, State};
+use rocket::{serde::json::Json, State};
 use validator::Validate;
 
 /// # Edit Server
@@ -16,7 +17,6 @@ use validator::Validate;
 #[patch("/<target>", data = "<data>")]
 pub async fn edit(
     db: &State<Database>,
-    account: Account,
     user: User,
     target: Reference<'_>,
     data: Json<v0::DataEditServer>,
